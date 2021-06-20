37da00ae353a8a27c71823c4e494a412e60be024
李相举 <lixiangju@imohoo.com>
2021-06-20 12:29:16 +0800

    完成本周作业

--- Week_16/jsx/main.js
+++ Week_16/jsx/main.js
@@ -1,14 +1,40 @@
 import { Carousel } from "./carousel.js"
 import { Component, createElement } from "./framework.js"
 import { Button } from "./button.js"
+import { List } from "./list.js"
 
 let pics = [
-  "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
-  "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
-  "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
-  "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg"
+  {
+    img: "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+    url: "https://www.baidu.com",
+    title: "Image1"
+  },
+  {
+    img: "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+    url: "https://www.baidu.com",
+    title: "Image2"
+  },
+  {
+    img: "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+    url: "https://www.baidu.com",
+    title: "Image3"
+  },
+  {
+    img: "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+    url: "https://www.baidu.com",
+    title: "Image4"
+  },
 ]
 
 // let aaa = <Carousel src={pics} onChange={event => console.log(event.detail.position)} onClick={event => console.log(event.detail.data)} />
-let aaa = <Button>会计法空间大烧烤架</Button>
+// let aaa = <Button>会计法空间大烧烤架</Button>
+let aaa = <List data={pics}>
+  {(item) =>
+    <div>
+      <img src={item.img}></img>
+      <a href={item.url}>{item.title}</a>
+    </div>
+  }
+
+</List>
 aaa.mountTo(document.body)
